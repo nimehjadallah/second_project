7511a65b325e56a5bb07d966e4de02110230fa3f
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-05-28 19:25:05 -0400

    removing put route!

--- routes/postApiRoutes.js
+++ routes/postApiRoutes.js
@@ -18,15 +18,15 @@ module.exports = function(app) {
         });
     });
 
-    app.put("/api/recipe", function(req, res) {
-        db.Recipe.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }.then(function(dbRecipe){
-                    res.json(dbRecipe);
-                })
-            });
-    });
+    // app.put("/api/recipe", function(req, res) {
+    //     db.Recipe.update(
+    //         req.body,
+    //         {
+    //             where: {
+    //                 id: req.body.id
+    //             }.then(function(dbRecipe){
+    //                 res.json(dbRecipe);
+    //             })
+    //         });
+    // });
 };
