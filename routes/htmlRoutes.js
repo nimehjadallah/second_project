149aaffde493f883dd45daaa5c0565d0f6d2c154
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-05-28 20:08:05 -0400

    heroku issues!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,13 +5,13 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/recipes.html"));
-  });
-
-  // app.get("/addRecipe", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/addRecipe.html"));
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/recipes.html"));
   // });
+
+  app.get("/addRecipe", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/addRecipe.html"));
+  });
 
   // app.get("/users", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/user.html"));
