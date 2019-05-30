422475c7f1c00da844ee3bdc34ef5f2b01664447
Nimeh Jadallah <nimeh_abueid@hotmail.com>
2019-05-30 19:24:06 -0400

    added readme and wokred on search bar

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# second_project
+# Second_project
+
+## Meet the Team
+* Grace Jadallah
+* Nason A Seng
+* Patrick  Marshall Caserta
+* Vinny Principe
+
+## Project Description
+**Our Recipes** is a cooking recipe website that will let users add their favorite recipes and search for recipes added by other users.
+
+
+
+## Deployment
+* See the Deployead app on Heroku
+[Our Recipes](https://damp-waters-30860.herokuapp.com/)
+
+
+  <!-- add a picture -->
+
+
+
--- schema.sql
+++ schema.sql
@@ -7,3 +7,11 @@
 DROP DATABASE IF EXISTS recipes_db;
 CREATE DATABASE recipes_db;
 
+Use recipes_db;
+INSERT INTO recipes (name, ingredients, steps, createdAt, updatedAt)
+Values ("Cilantro Pistachio Pesto Shrimp Pasta", "Shrimp, Pasta, Cilantro, Pistachios,jalapeño, garlic cloves, lime juice, olive oil, garlic powder,  salt and pepper. ",
+"Make the pesto by adding the following to the bowl of a food processor: cilantro, pistachios, jalapeño, garlic cloves, lime juice, olive oil, water and salt and pepper. Process until smooth, scraping down the sides and processing again, if necessary.
+Next cook the shrimp: add olive oil to a large skillet or pan and place over medium high heat. Add in shrimp, garlic powder and salt and pepper; cook until shrimp is no longer pink. Remove from heat and set aside.
+Cook the pasta until al dente, according to the directions on the package. Drain pasta, then add back to pot. Stir in the pesto and shrimp until well coated. Add pasta to bowls and garnish with goat cheese, a few 
+cilantro leaves and a sprinkle of crushed pistachios. Serve immediately. Makes about 4 servings.", now(), now());
+
